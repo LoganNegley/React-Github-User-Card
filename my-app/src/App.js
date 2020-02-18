ab787f178c18be0a204ebada9c4f6566b97855f2
Logan Negley <logannegley@gmail.com>
2020-02-17 18:21:08 -0600

    axios call made and failed

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios';
 
 
 class App extends React.Component {
   constructor() {
     super(); 
     this.state = { 
-       myData:'',
+       myData:[],
     }
     console.log(this.state)
   }
+
+  componentDidMount(){
+  axios.get('https://api.github.com/logannegley/')
+  .then (res => {
+  console.log(res)
+   })
+  .catch(err => console.log(err)
+  );
+}
 
     render(){
       return (
